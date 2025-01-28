6999aafcdaed88acdb986e5a9bcb3fccfd97c3e6
alurugnana <125435044+alurugnana@users.noreply.github.com>
2025-01-28 11:43:38 +0530

    Update log_config.py

--- app/core/log_config.py
+++ app/core/log_config.py
@@ -1,5 +1,5 @@
 import logging
 
-# Set up logging configuration
+# Set up logging config
 logging.basicConfig(level=logging.INFO, format="%(levelname)s:     %(message)s")
 logger = logging.getLogger()
